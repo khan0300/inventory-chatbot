290352cdc2ff53c34eedc052b393fa0c9844f1b6
Mat <matp@live.ca>
2016-11-20 03:34:05 -0500

    You can buy things and the stock will decrease.

--- routes/chatbot.js
+++ routes/chatbot.js
@@ -12,7 +12,7 @@ UPDATE: ADD A SESSION ID
 var express = require('express');
 var router = express.Router();
 var apiai = require('apiai');
-var app = apiai("");
+var app = apiai("59f3c81695f3494db7c0ddd341095db2");
 var Product = require('../models/product');
 
 
@@ -27,25 +27,46 @@ router.post('/send', function(req, res) {
 
   request.on('response', function(response){
 
+    var productName = response.result.parameters.product;
+
     switch(response.result.metadata.intentName) {
+
       case "ask about product availability":
-        Product.findOne({name: new RegExp('^'+response.result.parameters.product+'$', "i")}, (err, product)=>{
-          if (err || !product) res.send("Couldn't find a product with that name.");
+        Product.findOne({name: new RegExp('^'+productName+'$', "i")}, (err, product)=>{
+          if (err || !product) res.send("Couldn't find a product called \""+productName+"\".");
           else res.send("We have " + product.stock + " of the " + product.name + " in stock.");
         });
         break;
 
       case "ask for description":
-        Product.findOne({name: new RegExp('^'+response.result.parameters.product+'$', "i")}, (err, product)=>{
-          if (err || !product) res.send("Couldn't find a product with that name.");
+        Product.findOne({name: new RegExp('^'+productName+'$', "i")}, (err, product)=>{
+          if (err || !product) res.send("Couldn't find a product called \""+productName+"\".");
           else res.send(product.description);
         });
         break;
 
       case "ask about product cost":
-        Product.findOne({name: new RegExp('^'+response.result.parameters.product+'$', "i")}, (err, product)=>{
-          if (err || !product) res.send("Couldn't find a product with that name.");
-          else res.send("The " + product.name + " costs " + product.price + ".");
+        Product.findOne({name: new RegExp('^'+productName+'$', "i")}, (err, product)=>{
+          if (err || !product) res.send("Couldn't find a product called \""+productName+"\".");
+          else res.send("The " + product.name + " costs $" + product.price + ".");
+        });
+        break;
+
+      case "ask to buy":
+        Product.findOne({name: new RegExp('^'+productName+'$', "i")}, (err, product)=>{
+          if (err || !product) {
+            res.send("Couldn't find a product called \""+productName+"\".");
+          }
+          else {
+            if (product.stock==0) {
+              res.send("Unfortunately we are sold out!");
+            }
+            else {
+              product.stock -= 1;
+              product.save();
+              res.send("Thanks for purchasing the " + product.name + "!");
+            }
+          }
         });
         break;
 
